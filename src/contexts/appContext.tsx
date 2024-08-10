16ddf3cba3092f1464270afae1d8724b97e9680f
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-08-10 22:50:00 +0700

    update

--- src/contexts/appContext.tsx
+++ src/contexts/appContext.tsx
@@ -4,6 +4,7 @@ import React, {
   SetStateAction,
   useState,
 } from 'react';
+import { UserDto } from '@/dtos/User.dto';
 export type TypeAppState = {
   isOpenMenu: boolean;
   setIsOpenMenu: Dispatch<SetStateAction<boolean>> | undefined;
@@ -11,23 +12,31 @@ export type TypeAppState = {
   setIsOpenNavMenu: Dispatch<SetStateAction<boolean>> | undefined;
   settings?: Record<string, string>;
   setSettings?: Dispatch<SetStateAction<Record<string, string>>> | undefined;
+  user?: UserDto;
+  setUser?: Dispatch<SetStateAction<UserDto | undefined>> | undefined;
 };
 
 const AppContext = createContext<TypeAppState | undefined>(undefined);
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [user, setUser] = useState<UserDto | undefined>();
   const [isOpenNavMenu, setIsOpenNavMenu] = useState(false);
   const [settings, setSettings] = useState({});
+
   return (
-    <AppContext.Provider value={{
-      isOpenNavMenu,
-      setIsOpenNavMenu,
-      isOpenMenu,
-      setIsOpenMenu,
-      settings,
-      setSettings
-    }}>
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        isOpenNavMenu,
+        setIsOpenNavMenu,
+        isOpenMenu,
+        setIsOpenMenu,
+        settings,
+        setSettings,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,21 +1,23 @@
 import { UserDto } from '@/dtos/User.dto';
 import { getCookie, setCookie, deleteCookie } from 'cookies-next';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import AppContext from '@/contexts/appContext';
 
 export default function useUser() {
+  const appContext = useContext(AppContext);
   const router = useRouter();
   const pathnameNeedLogin = [
     '/gio-hang/thanh-toan',
     '/gio-hang/thanh-cong',
     '/tai-khoan/lich-su/[id]',
   ];
-  const [user, setUser] = useState<UserDto | null>(null);
+
   useEffect(() => {
     const user = getCookie('user');
     if (user) {
       const obj = JSON.parse(user);
-      setUser(obj);
+      appContext?.setUser && appContext.setUser(obj);
     } else {
       if (pathnameNeedLogin.includes(router.pathname)) {
         router.push('/tai-khoan/dang-nhap?redirectUrl=' + router.pathname);
@@ -23,7 +25,7 @@ export default function useUser() {
     }
   }, []);
   const setCookieUser = (user: UserDto) => {
-    setUser(user);
+    appContext?.setUser && appContext.setUser(user);
     setCookie('user', JSON.stringify(user), {
       maxAge: 86400,
       path: '/',
@@ -31,9 +33,9 @@ export default function useUser() {
   };
 
   const logout = () => {
-    setUser(null);
+    appContext?.setUser && appContext.setUser(undefined);
     deleteCookie('user');
   };
 
-  return { user, setCookieUser, logout };
+  return { user: appContext?.user, setCookieUser, logout };
 }
