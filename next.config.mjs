c56cecd5ab8d70b422a4945f64efc4d792ac137b
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-09-15 22:12:42 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,14 @@ const nextConfig = {
     },
   },
   images: {
-    domains: ['s3.ap-southeast-1.amazonaws.com','localhost','admin.mikiperfume.com','minhtuauthentic-be.hak-app.com', 'be.mikiperfume.com'],
+    domains: [
+      's3.ap-southeast-1.amazonaws.com',
+      'localhost',
+      'admin.mikiperfume.com',
+      'minhtuauthentic-be.hak-app.com',
+      'be.mikiperfume.com',
+      'be-new.mikiperfume.com'
+    ],
   },
   transpilePackages: ['antd', '@ant-design',  "rc-util",
     "@ant-design/icons",
