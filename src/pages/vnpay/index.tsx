c3f7b1c28022475adb4ea05fae8dc9d417211083
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-09-17 23:47:01 +0700

    update

--- src/pages/vnpay/index.tsx
+++ src/pages/vnpay/index.tsx
@@ -4,24 +4,62 @@ import Header from '@/components/organisms/header';
 import Footer from '@/components/organisms/footer';
 import Layout from '@/components/templates/Layout';
 import { ServerSideProps } from '@/config/type';
+import { hashVNPAY } from '@/utils/vnpay';
+import { ORDER_STATUS } from '@/config/enum';
 
-export const getServerSideProps = (async () => {
+export const getServerSideProps = async ({ query }: any) => {
   const resDefault = await getDefaultSeverSide();
+  let is_success = false;
+  const vnp_Params = query as Record<string, string>;
+  const secureHash = vnp_Params['vnp_SecureHash'];
+  if (vnp_Params?.vnp_SecureHashType) {
+    delete vnp_Params['vnp_SecureHashType'];
+  }
+  if (vnp_Params?.vnp_SecureHash) {
+    delete vnp_Params['vnp_SecureHash'];
+  }
+  const signed = hashVNPAY(vnp_Params);
+
+  if (secureHash === signed) {
+    is_success = true;
+    const order_id = vnp_Params['vnp_TxnRef'];
+    await fetch(`${process.env.BE_URL}/api/orders/${order_id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: order_id,
+        status: ORDER_STATUS.DONE,
+        note: 'VNPAY đã thanh toán thành công',
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('Success Payment ', res);
+      })
+      .catch((err) => {});
+  }
+
   return {
-    props: resDefault,
+    props: {
+      ...resDefault,
+      is_success,
+    },
   };
-}) satisfies GetServerSideProps<ServerSideProps>;
+};
 
 export default function VnPaySuccess({
   menu,
   footerContent,
   settings,
+  is_success,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Header settings={settings} menu={menu} />
       <Layout settings={settings} menu={menu}>
-        <h1>VNPAY SUCCESS</h1>
+        {is_success ? <h1>VNPAY SUCCESS</h1> : <h1>VNPAY FAIL</h1>}
       </Layout>
       <Footer footerContent={footerContent} />
     </>
