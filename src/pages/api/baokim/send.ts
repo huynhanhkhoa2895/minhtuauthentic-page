a45461123d2963f2fcc54304f472f97cc6148d6f
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-09-21 09:32:40 +0700

    update

--- src/pages/api/baokim/send.ts
+++ src/pages/api/baokim/send.ts
@@ -7,21 +7,21 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     const url = `${process.env.NEXT_PUBLIC_BAO_KIM_URL}/payment/api/v5/order/send`;
-    const auth = await jwt.sign(
-      {
-        iat: Math.floor(Date.now() / 1000),
-        exp: Math.floor(Date.now() / 1000) + 60 * 60,
-        iss: process.env.NEXT_PUBLIC_BAO_KIM_API,
-        aud: process.env.NEXT_PUBLIC_BAO_KIM_SECRET,
-      },
-      process.env.NEXT_PUBLIC_BAO_KIM_SECRET,
-    );
+    const sign = {
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      iss: process.env.NEXT_PUBLIC_BAO_KIM_API,
+      aud: process.env.NEXT_PUBLIC_BAO_KIM_SECRET,
+    };
+    const auth = await jwt.sign(sign, process.env.NEXT_PUBLIC_BAO_KIM_SECRET);
     console.log(
       'url baokim',
       process.env.NEXT_PUBLIC_BAO_KIM_API,
       process.env.NEXT_PUBLIC_BAO_KIM_SECRET,
       url,
     );
+    console.log('sign', sign);
+    console.log('jwt', auth);
     fetch(url, {
       method: 'POST',
       body: req.body,
