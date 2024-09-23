1eb099e10e9af74406a112a4023e16b7efaf4b82
Khoa Huynh <khoa.h@stor.ai>
2024-09-23 15:38:53 +0700

    update

---
+++ src/pages/api/orders/getordersById.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { handleDataFetch } from '@/utils/api';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    const query = req.query;
+    console.log('query test', query);
+    const url =
+      `${process.env.BE_URL}/api/pages/order/getById/` +
+      query.id;
+    const rs = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: req.body,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const _data = handleDataFetch(data, res);
+        res.status(200).json(_data);
+      })
+      .catch((error) => {
+        res
+          .status(error?.response?.statusCode || error?.status || 500)
+          .json(error);
+      });
+    res.status(200).json(rs);
+  }
+}
--- src/pages/ipn.tsx
+++ src/pages/ipn.tsx
@@ -1,4 +1,6 @@
 import { hashVNPAY } from '@/utils/vnpay';
+import { OrdersDto } from '@/dtos/Orders.dto';
+import { ORDER_STATUS } from '@/config/enum';
 
 export const getServerSideProps = async ({ res, query }: any) => {
   const vnp_Params = query as Record<string, string>;
@@ -11,13 +13,29 @@ export const getServerSideProps = async ({ res, query }: any) => {
   }
   const signed = hashVNPAY(vnp_Params);
   res.setHeader('Content-Type', 'application/json');
-  if (secureHash === signed) {
-    res.writeHead(200);
-    res.write(JSON.stringify({ RspCode: '00', Message: 'success' }));
+  const rs = await fetch(process.env.BE_URL+`/api/pages/order/getById/${vnp_Params['vnp_TxnRef']}`).then((res) => res.json());
+  const order: OrdersDto | undefined = rs?.data;
+  console.log('order', order);
+  if(
+    vnp_Params['vnp_ResponseCode'] === '00' ||
+    vnp_Params['vnp_ResponseCode'] === '99') {
+    if (!order?.id) {
+      res.write(JSON.stringify({ RspCode: '01', Message: 'Order not found' }));
+    } else if(Number(vnp_Params['vnp_Amount']) % 100 !== 0) {
+      res.write(JSON.stringify({ RspCode: '04', Message: 'Invalid amount' }));
+    } else if(vnp_Params['vnp_ResponseCode'] === '00' && order.status === ORDER_STATUS.DONE){
+      res.write(JSON.stringify({ RspCode: '02', Message: 'Order already confirmed' }));
+    } else {
+      if (secureHash === signed) {
+        res.write(JSON.stringify({ RspCode: '00', Message: 'success' }))
+      } else {
+        res.write(JSON.stringify({ RspCode: '97', Message: 'Fail checksum' }));
+      }
+    };
   } else {
-    res.writeHead(500);
-    res.write(JSON.stringify({ RspCode: '97', Message: 'Fail checksum' }));
+    res.write(JSON.stringify({ RspCode: '98', Message: 'Fail' }));
   }
+
   res.end();
   return {
     props: {},
