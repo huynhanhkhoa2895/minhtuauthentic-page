281042d910b13b0aaf8db6c318d43969c5f8d1af
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-09-29 01:24:38 +0700

    update

--- src/components/organisms/sectionSwiper/index.tsx
+++ src/components/organisms/sectionSwiper/index.tsx
@@ -149,18 +149,18 @@ const SectionSwiper = ({
         </div>
       ) : (
         <div className={'flex gap-3 p-3 bg-white my-3'}>
-          <Skeleton.Image
-            active={true}
-            style={{ width: '100px', height: '100px' }}
-          />
-          <Skeleton.Image
-            active={true}
-            style={{ width: '100px', height: '100px' }}
-          />
-          <Skeleton.Image
-            active={true}
-            style={{ width: '100px', height: '100px' }}
-          />
+          {/*<Skeleton.Image*/}
+          {/*  active={true}*/}
+          {/*  style={{ width: '100px', height: '100px' }}*/}
+          {/*/>*/}
+          {/*<Skeleton.Image*/}
+          {/*  active={true}*/}
+          {/*  style={{ width: '100px', height: '100px' }}*/}
+          {/*/>*/}
+          {/*<Skeleton.Image*/}
+          {/*  active={true}*/}
+          {/*  style={{ width: '100px', height: '100px' }}*/}
+          {/*/>*/}
         </div>
       )}
     </>
