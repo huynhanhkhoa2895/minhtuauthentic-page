78129a7e877b20bbb36c1175061ad836cfa69c41
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-09-29 01:34:06 +0700

    update

--- src/components/organisms/groupCategory/index.tsx
+++ src/components/organisms/groupCategory/index.tsx
@@ -37,100 +37,100 @@ const GroupCategory = ({
       className={'mt-3 mx-auto p-3 rounded-[10px]'}
       style={{ backgroundColor: staticComponent?.properties?.backgroundColor }}
     >
-      {/*<div className={'flex flex-col lg:flex-row justify-between mb-3 gap-2'}>*/}
-      {/*  <h2*/}
-      {/*    style={{ color: staticComponent?.properties?.textColor }}*/}
-      {/*    className={*/}
-      {/*      'w-max shrink-0 max-lg:items-center max-lg:flex max-lg:justify-between max-lg:w-full'*/}
-      {/*    }*/}
-      {/*  >*/}
-      {/*<Link*/}
-      {/*  className={'text-[18px] lg:text-[24px] uppercase font-bold '}*/}
-      {/*  href={generateSlugToHref(staticComponent?.category?.slugs?.slug)}*/}
-      {/*>*/}
-      {/*  {staticComponent?.title || staticComponent?.category?.name}*/}
-      {/*</Link>*/}
-      {/*<MobileView>*/}
-      {/*  <TagLink*/}
-      {/*    tagLinks={*/}
-      {/*      new TagLinkDto({*/}
-      {/*        id: 0,*/}
-      {/*        name: 'Xem tất cả',*/}
-      {/*        slug: generateSlugToHref(*/}
-      {/*          staticComponent?.category?.slugs?.slug,*/}
-      {/*        ),*/}
-      {/*      })*/}
-      {/*    }*/}
-      {/*    className={'lg:hidden last:mr-0 whitespace-nowrap text-black'}*/}
-      {/*  />*/}
-      {/*</MobileView>*/}
-      {/*</h2>*/}
-      {/*<div*/}
-      {/*  className={'flex justify-end gap-3 w-full lg:basis-[60%] lg:w-[70%] '}*/}
-      {/*>*/}
-      {/*  {tagLinks.length > 0 && (*/}
-      {/*    <div ref={ref} className={'w-full overflow-x-scroll '}>*/}
-      {/*      <div*/}
-      {/*        ref={refContainer}*/}
-      {/*        className={'flex gap-3 pb-2 justify-end w-max min-w-full'}*/}
-      {/*      >*/}
-      {/*        {tagLinks.map((tagLink, key: number) => {*/}
-      {/*          return (*/}
-      {/*            <TagLink*/}
-      {/*              key={key}*/}
-      {/*              tagLinks={tagLink}*/}
-      {/*              className={'last:mr-0 whitespace-nowrap'}*/}
-      {/*            />*/}
-      {/*          );*/}
-      {/*        })}*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  )}*/}
-      {/*  <TagLink*/}
-      {/*    className={'w-max max-lg:hidden shrink-0 h-[30px]'}*/}
-      {/*    tagLinks={*/}
-      {/*      new TagLinkDto({*/}
-      {/*        id: 0,*/}
-      {/*        name: 'Xem tất cả',*/}
-      {/*        slug: generateSlugToHref(*/}
-      {/*          staticComponent?.category?.slugs?.slug,*/}
-      {/*        ),*/}
-      {/*      })*/}
-      {/*    }*/}
-      {/*  />*/}
-      {/*</div>*/}
-      {/*</div>*/}
-      {/*<SectionSwiper*/}
-      {/*  isGrid={true}*/}
-      {/*  slidesPerView={5}*/}
-      {/*  spaceBetween={5}*/}
-      {/*  isUseHeightWrapper={true}*/}
-      {/*  renderItem={(item: unknown) => {*/}
-      {/*    const iProduct = item as IProductCategoryDto;*/}
-      {/*    const variantMap = new Map(*/}
-      {/*      iProduct.product?.variants?.map((variant) => [*/}
-      {/*        variant.is_default,*/}
-      {/*        variant,*/}
-      {/*      ]),*/}
-      {/*    );*/}
-      {/*    const variant = variantMap.get(true);*/}
-      {/*    if (!variant) {*/}
-      {/*      return;*/}
-      {/*    }*/}
-      {/*    return (*/}
-      {/*      iProduct.product && (*/}
-      {/*        <>*/}
-      {/*          /!*<ProductCard product={iProduct.product} variant={variant} />*!/*/}
-      {/*        </>*/}
-      {/*      )*/}
-      {/*    );*/}
-      {/*  }}*/}
-      {/*  data={*/}
-      {/*    staticComponent?.category?.products?.filter(*/}
-      {/*      (item: IProductCategoryDto) => item.product,*/}
-      {/*    ) || []*/}
-      {/*  }*/}
-      {/*/>*/}
+      <div className={'flex flex-col lg:flex-row justify-between mb-3 gap-2'}>
+        <div
+          style={{ color: staticComponent?.properties?.textColor }}
+          className={
+            'w-max shrink-0 max-lg:items-center max-lg:flex max-lg:justify-between max-lg:w-full'
+          }
+        >
+          <h2 style={{ color: staticComponent?.properties?.textColor }}>
+            <Link
+              className={'text-[18px] lg:text-[24px] uppercase font-bold '}
+              href={generateSlugToHref(staticComponent?.category?.slugs?.slug)}
+            >
+              {staticComponent?.title || staticComponent?.category?.name}
+            </Link>
+          </h2>
+          <MobileView>
+            <TagLink
+              tagLinks={
+                new TagLinkDto({
+                  id: 0,
+                  name: 'Xem tất cả',
+                  slug: generateSlugToHref(
+                    staticComponent?.category?.slugs?.slug,
+                  ),
+                })
+              }
+              className={'lg:hidden last:mr-0 whitespace-nowrap text-black'}
+            />
+          </MobileView>
+        </div>
+        <div
+          className={'flex justify-end gap-3 w-full lg:basis-[60%] lg:w-[70%] '}
+        >
+          {tagLinks.length > 0 && (
+            <div ref={ref} className={'w-full overflow-x-scroll '}>
+              <div
+                ref={refContainer}
+                className={'flex gap-3 pb-2 justify-end w-max min-w-full'}
+              >
+                {tagLinks.map((tagLink, key: number) => {
+                  return (
+                    <TagLink
+                      key={key}
+                      tagLinks={tagLink}
+                      className={'last:mr-0 whitespace-nowrap'}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          <TagLink
+            className={'w-max max-lg:hidden shrink-0 h-[30px]'}
+            tagLinks={
+              new TagLinkDto({
+                id: 0,
+                name: 'Xem tất cả',
+                slug: generateSlugToHref(
+                  staticComponent?.category?.slugs?.slug,
+                ),
+              })
+            }
+          />
+        </div>
+      </div>
+      <SectionSwiper
+        isGrid={true}
+        slidesPerView={5}
+        spaceBetween={5}
+        isUseHeightWrapper={true}
+        renderItem={(item: unknown) => {
+          const iProduct = item as IProductCategoryDto;
+          const variantMap = new Map(
+            iProduct.product?.variants?.map((variant) => [
+              variant.is_default,
+              variant,
+            ]),
+          );
+          const variant = variantMap.get(true);
+          if (!variant) {
+            return;
+          }
+          return (
+            iProduct.product && (
+              <ProductCard product={iProduct.product} variant={variant} />
+            )
+          );
+        }}
+        data={
+          (staticComponent?.category?.products || [])?.filter(
+            (item: IProductCategoryDto) => item.product,
+          ) || []
+        }
+      />
     </div>
   );
 };
--- src/components/organisms/home/homeCategory.tsx
+++ src/components/organisms/home/homeCategory.tsx
@@ -12,19 +12,19 @@ const HomeCategory = ({
 }) => {
   return (
     <>
-      {/*{(homeCategory || []).map((item: StaticComponentDto, key: number) => {*/}
-      {/*  const isActive = key === 0 || (key + 1) % 3 === 0;*/}
-      {/*  return (*/}
-      {/*    <Fragment key={'GroupCategory_' + key}>*/}
-      {/*      <GroupCategory staticComponent={item} />*/}
-      {/*      <BannerUnderCategory*/}
-      {/*        key={key + 'banner-under-category'}*/}
-      {/*        contents={bannerUnderCategory || []}*/}
-      {/*        position={key === 0 ? 0 : (key + 1) / 3}*/}
-      {/*      />*/}
-      {/*    </Fragment>*/}
-      {/*  );*/}
-      {/*})}*/}
+      {(homeCategory || []).map((item: StaticComponentDto, key: number) => {
+        const isActive = key === 0 || (key + 1) % 3 === 0;
+        return (
+          <Fragment key={'GroupCategory_' + key}>
+            <GroupCategory staticComponent={item} />
+            <BannerUnderCategory
+              key={key + 'banner-under-category'}
+              contents={bannerUnderCategory || []}
+              position={key === 0 ? 0 : (key + 1) / 3}
+            />
+          </Fragment>
+        );
+      })}
     </>
   );
 };
