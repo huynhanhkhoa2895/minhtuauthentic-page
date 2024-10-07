3b72fa6aa0c00ce477ad05cbfd02eb51c89f24c6
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-10-07 22:26:04 +0700

    update

--- src/components/organisms/news/detail.tsx
+++ src/components/organisms/news/detail.tsx
@@ -4,25 +4,21 @@ import { NewsDto } from '@/dtos/News.dto';
 
 type Props = {
   news: NewsDto;
-}
+};
 
-export default function NewsDetail({news}: Props){
-  return <>
-    {news && <NewsClock item={news} />}
-    {news?.images?.[0] && (
-      <div className={'w-full lg:w-[650px] p-3 mx-auto'}>
-        <ImageWithFallback
-          image={news?.images?.[0]?.image}
-        />
-      </div>
-    )}
-    <div
-      className={'container-html'}
-      dangerouslySetInnerHTML={{ __html: news?.description || '' }}
-    />
-    <div
-      className={'container-html'}
-      dangerouslySetInnerHTML={{ __html: news?.content || '' }}
-    />
-  </>
+export default function NewsDetail({ news }: Props) {
+  return (
+    <>
+      {news && <NewsClock item={news} />}
+      {news?.images?.[0] && (
+        <div className={'w-full lg:w-[650px] p-3 mx-auto'}>
+          <ImageWithFallback image={news?.images?.[0]?.image} />
+        </div>
+      )}
+      <div
+        className={'container-html'}
+        dangerouslySetInnerHTML={{ __html: news?.content || '' }}
+      />
+    </>
+  );
 }
