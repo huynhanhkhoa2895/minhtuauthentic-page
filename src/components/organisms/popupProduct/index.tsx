2b845bda52496574057029e7cc3b8f3c784d10d1
Huynh Anh Khoa <huynhanhkhoa2895@gmail.com>
2024-11-21 01:04:27 +0700

    update

--- src/components/organisms/popupProduct/index.tsx
+++ src/components/organisms/popupProduct/index.tsx
@@ -25,6 +25,7 @@ export default function PopupProduct() {
       );
     } else {
       setData(null);
+      appCtx?.setIsOpenPopupProduct && appCtx.setIsOpenPopupProduct(null);
     }
   }, [isModalOpen]);
 
