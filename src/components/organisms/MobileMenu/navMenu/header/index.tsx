afcf4ee86dbfccecdbde7d55d78729af9d7d6409
huynhkhoa <huynhanhkhoa2895@gmail.com>
2025-03-10 22:08:53 +0700

    update

--- src/components/organisms/MobileMenu/navMenu/header/index.tsx
+++ src/components/organisms/MobileMenu/navMenu/header/index.tsx
@@ -71,7 +71,7 @@ export default function NavMenuHeader({
     };
   }, []);
   return (
-    <div className={'relative h-[128px]'}>
+    <div className={'relative max-lg:h-[128px]'}>
       <NavbarMenuListButton className={className} settings={settings} />
       {isMobile && (
         <div
